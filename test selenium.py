4e3d32ad220e36ecf66899d597004ebae6aa3102
LidiiaMelnyk95 <lidiia.melnyk@gmail.com>
2021-06-03 17:46:11 +0200

    -upd

--- test selenium.py
+++ test selenium.py
@@ -8,13 +8,18 @@ fireFoxOptions = webdriver.FirefoxOptions()
 fireFoxOptions.headless = True
 driver = webdriver.Firefox(executable_path= '/Users/lidiiamelnyk/Documents/korrespondent/geckodriver', options=fireFoxOptions)
 driver.get(urlpage)
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var "
-                      "lenOfPage=document.body.scrollHeight;return lenOfPage;")
+#driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var "
+#                      "lenOfPage=document.body.scrollHeight;return lenOfPage;")
 # sleep for 3s
-time.sleep(15)
+#time.sleep(15)
 button = driver.find_element_by_css_selector('a[onclick="loadComments()"]')
 button.click()
 results = driver.find_element_by_xpath('//*[@id="frm"]/div[3]/div[1]/div[7]/div/div[2]/div[10]/div[3]/div/div/div[2]').text
 name =  driver.find_element_by_xpath('//*[@id="frm"]/div[3]/div[1]/div[7]/div/div[2]/div[10]/div[3]/div/div/div[1]/a').text
-#date = driver.find_element_by_xpath(' — 27.05.2021, 17:44        ')
-print(results, name)
+date = driver.find_element_by_xpath('//*[@id="frm"]/div[3]/div[1]/div[7]/div/div[2]/div[10]/div[3]/div/div/div[1]').text[(len(driver.find_element_by_xpath('//*[@id="frm"]/div[3]/div[1]/div[7]/div/div[2]/div[10]/div[3]/div/div/div[1]').text) - 17):]
+
+print('Text is: {}, '
+      'name is : {}, '
+      'date is: {}'.format(results,name,date))
+
+driver.quit()
