3ecc13ecef21b07e4532db02e5b9c11a0baa074d
LidiiaMelnyk95 <73523072+LidiiaMelnyk95@users.noreply.github.com>
2021-06-08 16:00:50 +0200

    -upd

--- korrespondent scrapper requests.py
+++ korrespondent scrapper requests.py
@@ -36,6 +36,7 @@ def main():
         response = requests.get(url, timeout=60)
         content = BeautifulSoup(response.content, "html.parser")
         main_div = content.find('div', attrs="articles-list")
+        news_sections = None
         try:
             news_sections = main_div.find_all('div', attrs="article article_rubric_top")
         except AttributeError:
@@ -44,7 +45,7 @@ def main():
         for news in news_sections:
             url_news = news.find('a').get('href')
             url_array.append(url_news)
-            url_array = list(dict.fromkeys(url_array))
+            #url_array = list(dict.fromkeys(url_array))
             print("URL array length {}".format(len(url_array)))
     url_array = set(url_array)
     try:
@@ -60,27 +61,58 @@ def main():
 
 
 class GetData():
-    def __init__(self, url, info_id, text):
+    def __init__(self, url, info_id, text, comments, author, date):
         self.url = 'https://cmt.korrespondent.net/comments/list.hnd'
         self.info_id = self.get_info_id_from_url( )
         self.text = self.get_data( )
 
-    def get_info_id_from_url(url):
+    def init_url(self, url):
+        url = 'https://cmt.korrespondent.net/comments/list.hnd'
+        return url
+    def get_info_id_from_url(self, url):
         info_id = re.findall("\d+", url)[0]
         return info_id
 
-    def get_data(info_id):
+    def get_data(self, info_id):
         parameters = {"lotId": "10", "sort": 1, "page": 0, "size": 20, "callback": None, "infoId": info_id}
-        response = requests.get(url, params=parameters)
+        response = requests.get(self.url, params=parameters)
         return response.content.decode("utf-8")
 
-    def parsing(text):
+    def parsing(self, text):
         json_string = text[1:-1]
         json_object = json.loads(json_string)
         comments_array = []
         for item in json_object["items"]:
             comments_array.append({"comment": item["text"], "username": item["userName"], "date": item["date"]})
         return comments_array
+
+def scrape_text(url):
+    info_id = GetData.get_info_id_from_url(url)
+    data_request = GetData.get_data(info_id)
+    comments = GetData.parsing(data_request).get('comment')
+    author_name = GetData.parsing(data_request).get('username')
+    date = GetData.parsing(data_request).get('date')
+    rows = []
+    df1 = pd.DataFrame(columns=columns_names)
+    rows.append({'url': url, 'comment': comments, 'date': date,
+                     'name': author_name})
+    df1 = df1.append(rows, ignore_index=True)
+    df1 = df1.drop_duplicates()
+    count = df1['comment'].count()
+    if count <= 0:
+        return
+    filename = "/Users/lidiiamelnyk/Documents/korrespondent/" + str(hash(url)) + '.csv'
+    with open(filename, 'w+', encoding='utf-8-sig',
+              newline='') as file:
+        df1.to_csv(file, sep=',', na_rep='', float_format=None,
+                   columns=['url', 'comment', 'date', 'name'],
+                   header=True, index=False, index_label=None,
+                   mode='a', compression='infer',
+                   quoting=None, quotechar='"', line_terminator=None, chunksize=None,
+                   date_format=None, doublequote=True, escapechar=None, decimal='.')
+        print("Finished writing to {}".format(filename))
+        file.close()
+    return "Comments count {}".format(count)
 
 
 if __name__ == '__main__':
