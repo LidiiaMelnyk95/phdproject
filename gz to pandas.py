cf8baa24f41d72e12c30d08ae872bbaec256b8a2
LidiiaMelnyk95 <73523072+LidiiaMelnyk95@users.noreply.github.com>
2021-08-10 08:20:11 +0200

    -upd

--- gz to pandas.py
+++ gz to pandas.py
@@ -7,17 +7,15 @@ import json
 pd.set_option('display.max_columns', 500)
 
 all_files = glob.glob('/Users/lidiiamelnyk/Documents/GitHub/COVID-19-TweetIDs/2020-01/*.jsonl.gz')
-lines = []
-dataframe1 = pd.DataFrame()
+
 for filename in all_files:
     with gzip.open(filename, 'rt') as f:
         data = pd.DataFrame(f.read().splitlines(),columns = ['json'], index = None)
        # data = data['json'].apply(json.loads)
         df_final = pd.json_normalize(data['json'].apply(json.loads))
-        dataframe1 = dataframe1.append(df_final)
         filename = filename[:-6] + '.csv'
         try:
-            df_ukrainian = dataframe1[dataframe1["lang"] == 'uk']
+            df_ukrainian = df_final[df_final["lang"] == 'uk']
             with open(filename, 'w+', encoding='utf-8-sig', newline='') as file:
                 df_ukrainian.to_csv(file, sep=',', na_rep='', float_format=None,
                            columns= True,
